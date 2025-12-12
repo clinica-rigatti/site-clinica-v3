f94198c8603b87470d1166846982bf426df88334
Desenvolvimento 2 <desenvolvimento@clinicarigatti.com.br>
2025-12-12 16:47:56 -0300

    Alterações no site

--- scripts/teamCarouselController.js
+++ scripts/teamCarouselController.js
@@ -18,7 +18,6 @@
     { name: 'CLEIDIANE CUBAS',    role: 'CONCIERGE',            image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/cleidiane.webp'      },
     { name: 'BRUNO LEON',         role: 'RECOMPRA',             image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/bruno.webp'          },
     { name: 'LARISSA PADULA',     role: 'SOCIAL MEDIA',         image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/larissa-padula.webp' },
-    { name: 'HENRIQUE',           role: 'DIRETOR DE MARKETING', image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/henrique.webp'       },
     { name: 'LUCAS HULSE',        role: 'VIDEOMAKER',           image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/lucas.webp'          },
     { name: 'IGOR',               role: 'DESIGNER',             image: 'https://clinicarigatti.s3.sa-east-1.amazonaws.com/imagens-site-v3/time/igor.webp'           }
   ];
